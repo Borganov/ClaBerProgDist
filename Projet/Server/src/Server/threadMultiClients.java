2e6ec750658ed5c99e3faf625e721e61aa2bd70b
Yann <yann.cla@gmail.com>
2018-06-02 19:26:50 +0200

    sysouts

--- Projet/Server/src/Server/threadMultiClients.java
+++ Projet/Server/src/Server/threadMultiClients.java
@@ -10,6 +10,8 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.ListIterator;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class threadMultiClients implements Runnable {
 	private Socket clientSocketOnServer;
 	private user u;
@@ -143,14 +145,23 @@ public class threadMultiClients implements Runnable {
 
 							String downloadIp = null;
 							String userName = dbm.getUserNameByUserId(downloadUserId);
+							System.out.println("userName" + userName);
 
 							ListIterator<user> liu = usersList.usersList.listIterator();
 							while(liu.hasNext()){
 								if(liu.next().getId().equals(userName))
 								{
 									downloadIp = usersList.usersList.get(liu.nextIndex()-1).getIp();
+									System.out.println("ip : " + usersList.usersList.get(liu.nextIndex()-1).getIp());
+
+									System.out.println("id : " + usersList.usersList.get(liu.nextIndex()-1).getId());
 								}
+
+
 							}
+							
+							System.out.println(downloadIp);
+							System.out.println(downloadPath);
 							
 							pout.println(downloadIp);
 							pout.flush();
