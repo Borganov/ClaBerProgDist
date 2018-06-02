cc312f869cc33436e38456b14d8d6927f73fc6a6
Alex <berclazalexandre@gmail.com>
2018-06-02 10:06:19 +0200

    Mise à niveau de la partie serveur

---
+++ Projet/Server/src/Server/dbManager.java
@@ -0,0 +1,62 @@
+package Server;
+
+import java.sql.*;
+public class dbManager {
+	
+	private Connection con;
+	private Statement stmt;
+
+	public dbManager() throws java.lang.Exception{
+		Class.forName("org.hsqldb.jdbcDriver");
+		
+		String url ="jdbc:hsqldb:/c:/temp/projetdb_file";
+		
+		con = DriverManager.getConnection(url, "sa", "");
+		
+		
+		stmt=con.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+		
+		/*Création de la db avec les deux tables user et file*/
+		stmt.execute(
+				"CREATE TABLE USER (U_ID INTEGER IDENTITY PRIMARY KEY, "
+				+"U_NAME VARCHAR(50) NOT NULL, "
+				+"U_PASSWORD VARCHAR(50) NOT NULL, )"
+				);
+		
+		stmt.execute(
+				"CREATE TABLE FILE (F_ID INTEGER IDENTITY PRIMARY KEY , "
+				+"F_NAME VARCHAR(50) NOT NULL, "
+				+"F_PATH VARCHAR(150) NOT NULL, "
+				+ "U_ID INTEGER NOT NULL, "
+				+ "FOREIGN KEY (U_ID) REFERENCES USER(U_ID))");
+		
+		stmt.execute(
+				"INSERT INTO USER(U_NAME, U_PASSWORD)"
+				+"VALUES('Alex', '123')");
+		
+		stmt.execute(
+				"INSERT INTO USER(U_NAME, U_PASSWORD)"
+				+"VALUES('Yann', '123')");
+		
+		stmt.execute(
+				"INSERT INTO USER(U_NAME, U_PASSWORD)"
+				+"VALUES('Valentin', '123')");
+		
+		
+		stmt.execute("SELECT * FROM USER");
+		
+		
+		ResultSet rs = stmt.getResultSet();
+		
+		while(rs.next()) {
+			int id = rs.getInt("U_ID");
+			String name = rs.getString("U_NAME");
+			String password = rs.getString("U_PASSWORD");
+			System.out.println(id + " " +name+ " " +password);
+
+		}
+		
+		stmt.close();
+
+	}
+}
---
+++ Projet/Server/src/Server/serverMain.java
@@ -0,0 +1,83 @@
+package Server;
+
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class serverMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			dbManager dbm = new dbManager();
+			System.out.println("Ok db créé");
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		Enumeration<NetworkInterface> allNetworkInterfaces;
+		Enumeration<InetAddress> allInetAddresses;
+		InetAddress serverAddress = null;
+
+		ServerSocket mySkserver;
+		Socket srvSocket;
+
+		// initialisation du port1
+		int port1 = 45000;
+
+		// définition de l'address du server
+		try {
+			allNetworkInterfaces = NetworkInterface.getNetworkInterfaces();
+
+			while (allNetworkInterfaces.hasMoreElements()) {
+				NetworkInterface ni = allNetworkInterfaces.nextElement();
+
+				allInetAddresses = ni.getInetAddresses();
+
+				while (allInetAddresses.hasMoreElements()) {
+					InetAddress ia = allInetAddresses.nextElement();
+
+					if (!ia.isLoopbackAddress()) {
+						if (!ia.isLinkLocalAddress()) {
+							serverAddress = ia;
+						}
+					}
+				}
+			}
+
+			System.out.println("Démarrage du serveur " + serverAddress + " sur le port " + port1);
+
+			mySkserver = new ServerSocket(port1, 5, serverAddress);
+			usersList usersList = new usersList();
+			
+			while (true) {
+				Socket clientSocket = mySkserver.accept();
+				Thread multiClients = new Thread(new threadMultiClients(clientSocket, usersList));
+
+				multiClients.start();
+
+			}
+
+			// mySkserver.close();
+			// clientSocket.close();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
